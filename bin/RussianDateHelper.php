f8aa5d0ca9f6153b2ea015ad54b42d1086ffe1d1
Lakremon <self@lakremon.ru>
2018-03-08 10:31:02 +0700

    fixes

--- bin/RussianDateHelper.php
+++ bin/RussianDateHelper.php
@@ -6,15 +6,52 @@ namespace russianDateHelper;
  * Class russianDateHelper - main helper class
  * @package russianDateHelper
  */
-class russianDateHelper
+class RussianDateHelper
 {
     protected static $_countOfPeriods = 2;
 
+    const PERIODS_TOMES = [
+        'year' => 365 * 24 * 60 * 60,
+        'month' => 30 * 24 * 60 * 60,
+        'week' => 7 * 24 * 60 * 60,
+        'day' => 24 * 60 * 60,
+        'hour' => 60 * 60,
+        'minute' => 60,
+        'second' => 1,
+    ];
+
+    const PERIODS_NAMES = [
+        'year' => ['год', 'года', 'лет'],
+        'month' => ['месяц', 'месяца', 'месяцев'],
+        'week' => ['неделя', 'недели', 'недель'],
+        'day' => ['день', 'дня', 'дней'],
+        'hour' => ['час', 'часа', 'часов'],
+        'minute' => ['минута', 'минуты', 'минут'],
+        'second' => ['секунда', 'секунды', 'секунд'],
+    ];
+
+    /**
+     * Convert \DateTime or \DatePeriod input var to integer
+     * @param $data integer | \DatePeriod | \DateTime
+     * @return $data integer
+     */
+    protected static function prepareInData($data)
+    {
+        if (is_int($data)) {
+            return $data;
+        }
+        if ($data instanceof \DatePeriod::class){
+            return $data
+        }
+
+    }
+
     /**
      * Set how match count of periods show in result string
-     * @param $countOfPeriodValue
+     * @param $countOfPeriodValue integer|\DateTime
+     * @return $timeInSeconds integet
      */
-    public static function __setCountOfPeriods($countOfPeriodValue)
+    public static function setCountOfPeriods($countOfPeriodValue)
     {
         if (!is_integer($countOfPeriodValue)) {
             // TODO add wrong property type exception
@@ -27,55 +64,38 @@ class russianDateHelper
 
     /**
      * Get time period as count of seconds and return user-friendly period format
-     * @param $periodInSeconds integer
+     * @param $periodInSeconds integer|\DateTime
      * @return string
      */
-    public static function timeToPeriod($periodInSeconds)
+    public static function differenceToPeriod($period)
     {
-        if (!is_integer($periodInSeconds)) {
+        if (!is_integer($period)) {
+            $periodInSeconds = $period;
             // TODO add wrong property type exception
         }
         $result = '';
-        $periodsTomes = [
-                'year' => 365 * 24 * 60 * 60,
-                'month' => 30 * 24 * 60 * 60,
-                'week' => 7 * 24 * 60 * 60,
-                'day' => 24 * 60 * 60,
-                'hour' => 60 * 60,
-                'minute' => 60,
-                'second' => 1,
-        ];
-        $periodNames = [
-                'year' => ['год', 'года', 'лет'],
-                'month' => ['месяц', 'месяца', 'месяцев'],
-                'week' => ['неделя', 'недели', 'недель'],
-                'day' => ['день', 'дня', 'дней'],
-                'hour' => ['час', 'часа', 'часов'],
-                'minute' => ['минута', 'минуты', 'минут'],
-                'second' => ['секунда', 'секунды', 'секунд'],
-        ];
 
-        $i = 0;
-        foreach ($periodsTomes as $periodKey => $periodValue) {
-            if ($i < self::$_countOfPeriods) {
+        $iteration = 0;
+        foreach (self::PERIODS_TOMES as $periodKey => $periodValue) {
+            if ($iteration < self::$_countOfPeriods) {
                 $currentPeriodValue = floor($periodInSeconds / $periodValue);
                 if ($currentPeriodValue) {
                     $result .= $currentPeriodValue . ' ';
                     if ($currentPeriodValue % 10 > 4) {
-                        $result .= $periodNames [$periodKey][2];
+                        $result .= self::PERIODS_NAMES[$periodKey][2];
                     } elseif ($currentPeriodValue % 10 == 1 && ($currentPeriodValue % 100 < 10 || $currentPeriodValue % 100 > 20)) {
-                        $result .= $periodNames [$periodKey][0];
+                        $result .= self::PERIODS_NAMES[$periodKey][0];
                     } else {
-                        $result .= $periodNames [$periodKey][1];
+                        $result .= self::PERIODS_NAMES[$periodKey][1];
                     }
                     $periodInSeconds %= $periodValue;
-                    $i++;
-                    $result .= $i == self::$_countOfPeriods ? '.' : ' ';
+                    $iteration++;
+                    $result .= $iteration == self::$_countOfPeriods ? '.' : ' ';
                 }
             } else {
                 break;
             }
         }
-        return $result;
+        return trim($result);
     }
 }
--- examples/tests.php
+++ examples/tests.php
@@ -5,3 +5,6 @@
  * Date: 3/7/18
  * Time: 4:44 PM
  */
+require_once '../bin/RussianDateHelper.php';
+
+var_dump(russianDateHelper\RussianDateHelper::timeToPeriod(3 * 24 * 60 * 60));
